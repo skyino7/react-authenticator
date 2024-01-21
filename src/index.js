daeed2f4a4f317234739900739dd939985325d49
Seth Ketsi <christambassador12@gmail.com>
2024-01-21 23:17:06 +0000

    Testing the react authentication with auth0

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  <Auth0Provider
+    domain="dev-zqtnkzw3ej1ke2ga.us.auth0.com"
+    clientId="oqYcYSlbsyfm1L9mJNPRxUrdv5sOrKCw"
+    redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
